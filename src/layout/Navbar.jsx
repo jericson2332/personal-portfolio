003552e9fdc0646d907975a8bf7580872b2c094f
JERICSON AMBROCIO LAYUGAN <91362655+jericson2332@users.noreply.github.com>
2026-05-18 16:22:55 +0800

    Updated

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -44,7 +44,9 @@ export const Navbar = () =>{
 
                 {/* CTA Button */}
                 <div className="hidden md:block">
-                    <Button size ="sm">Contact Me</Button>
+                    <a href="#contact">
+                        <Button size ="sm">Contact Me</Button>
+                    </a>
                 </div>
 
                 {/* Mobile VIew Button*/}
@@ -65,7 +67,9 @@ export const Navbar = () =>{
                                 {link.label}
                             </a>
                         ))}
-                        <Button onClick = {() => setIsMobileMenuOpen(false)}>Contact Me</Button>
+                        <a href="#contact">
+                            <Button onClick = {() => setIsMobileMenuOpen(false)}>Contact Me</Button>
+                        </a>
                 </div>
             </div>
             )}
